fdb46b6cd8d5c4ffe0f423b5e26dee6f490036e9
Dave Haigh <davehaigh@outlook.com>
2019-09-20 12:36:59 +0100

    1-0 course and date pages

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,21 @@ var _myData = {
         "employer-1": require(__dirname + '/data/employer-1.json')
     },
     "defaultAccountID": "employer-1",
+    "courses": require(__dirname + '/data/courses.json'),
+    "startDates": [
+        {
+            "id": "aug2019",
+            "name": "August 2019"
+        },
+        {
+            "id": "sep2019",
+            "name": "September 2019"
+        },
+        {
+            "id": "oct2019",
+            "name": "October 2019"
+        }
+    ]
 }
 
 // Add your routes here - above the module.exports line
--- app/routes/1-0/routes.js
+++ app/routes/1-0/routes.js
@@ -74,6 +74,67 @@ module.exports = function (router,_myData) {
             myData:req.session.myData
         });
     });
+    // TODO
+    router.post('/' + version + '/funding-choose-course', function (req, res) {
+        var _account = req.session.myData.accounts[req.session.myData.accountID],
+            _courses = req.session.myData.courses.list
+        // Answer
+        req.session.myData.whichCourseAnswerTemp = req.body.whichCourseAnswer
+        //Set default answer if includeValidation is false and no answer given
+        if(req.session.myData.includeValidation == "false"){
+            req.session.myData.whichCourseAnswerTemp = req.session.myData.whichCourseAnswerTemp || _courses[0].value
+        }
+        // Validation
+        if(!req.session.myData.whichCourseAnswerTemp) {
+            req.session.myData.validationError = "true"
+            req.session.myData.validationErrors.whichCourseAnswer = {
+                "anchor": "whichCourse-1",
+                "message": "Select a course"
+            }
+        }
+        // Next action
+        if(req.session.myData.validationError == "true") {
+            res.render(version + '/funding-choose-course', {
+                myData: req.session.myData
+            });
+        } else {
+            req.session.myData.whichCourseAnswer = req.session.myData.whichCourseAnswerTemp
+            res.redirect(301, '/' + version + '/funding-choose-start-date');
+        }
+    });
+
+    // Choose start date
+    router.get('/' + version + '/funding-choose-start-date', function (req, res) {
+        res.render(version + '/funding-choose-start-date', {
+            myData:req.session.myData
+        });
+    });
+    router.post('/' + version + '/funding-choose-start-date', function (req, res) {
+        var _account = req.session.myData.accounts[req.session.myData.accountID]
+        // Answer
+        req.session.myData.whichStartDateAnswerTemp = req.body.whichStartDateAnswer
+        //Set default answer if includeValidation is false and no answer given
+        if(req.session.myData.includeValidation == "false"){
+            req.session.myData.whichStartDateAnswerTemp = req.session.myData.whichStartDateAnswerTemp || req.session.myData.startDates[0].id
+        }
+        // Validation
+        if(!req.session.myData.whichStartDateAnswerTemp) {
+            req.session.myData.validationError = "true"
+            req.session.myData.validationErrors.whichStartDateAnswer = {
+                "anchor": "whichStartDate-1",
+                "message": "Select a start date"
+            }
+        }
+        // Next action
+        if(req.session.myData.validationError == "true") {
+            res.render(version + '/funding-choose-start-date', {
+                myData: req.session.myData
+            });
+        } else {
+            req.session.myData.whichStartDateAnswer = req.session.myData.whichStartDateAnswerTemp
+            res.redirect(301, '/' + version + '/TODO');
+        }
+    });
 
 
  };
