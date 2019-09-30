1c52b07ed1dfcdbcfaf9afedf445eaf9dc66a538
Dave Haigh <davehaigh@outlook.com>
2019-09-30 14:49:23 +0100

    14-0 fixed mvs start date options

--- app/routes.js
+++ app/routes.js
@@ -18,17 +18,20 @@ var _myData = {
         {
             "id": "aug2019",
             "name": "August 2019",
-            "range": "Aug 2019 to Oct 2019"
+            "range": "Aug 2019 to Oct 2019",
+            "empmvs": true
         },
         {
             "id": "sep2019",
             "name": "September 2019",
-            "range": "Sep 2019 to Nov 2019"
+            "range": "Sep 2019 to Nov 2019",
+            "empmvs": true
         },
         {
             "id": "oct2019",
             "name": "October 2019",
-            "range": "Oct 2019 to Dec 2019"
+            "range": "Oct 2019 to Dec 2019",
+            "empmvs": true
         },
         {
             "id": "nov2019",
