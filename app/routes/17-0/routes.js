d55b11ff94d73508232b37733cad8df61ff4d6fc
Dave Haigh <davehaigh@outlook.com>
2019-10-31 08:49:48 +0000

    jan feb march dates showing

--- app/routes/17-0/routes.js
+++ app/routes/17-0/routes.js
@@ -354,25 +354,19 @@ module.exports = function (router,_myData) {
                 "id": "oct2019",
                 "name": "October 2019",
                 "range": "Oct 2019 to Dec 2019",
-                "endDate": new Date("01-01-2020"),
-                "empmvs": true,
-                "promvs": true
+                "endDate": new Date("01-01-2020")
             },
             {
                 "id": "nov2019",
                 "name": "November 2019",
                 "range": "Nov 2019 to Jan 2020",
-                "endDate": new Date("02-01-2020"),
-                "promvs": true,
-                "empmvs": true
+                "endDate": new Date("02-01-2020")
             },
             {
                 "id": "dec2019",
                 "name": "December 2019",
                 "range": "Dec 2019 to Feb 2020",
-                "endDate": new Date("03-01-2020"),
-                "promvs": true,
-                "empmvs": true
+                "endDate": new Date("03-01-2020")
             },
             {
                 "id": "jan2020",
